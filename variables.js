1c3f784c41000dfc29b01fe0e4956d400436705f
Zac Walters <zacwalters4@gmail.com>
2022-08-02 15:37:02 -0600

    Change string variables and add output

--- variables.js
+++ variables.js
@@ -1,17 +1,26 @@
-# Strings
+
 
 var email = "zacwalters4@gmail.com"
-var name = zac
-var lastName = walters
+var name = "zac"
+var lastName = "walters"
 
-#Numbers
+
 
 var age = 24
 var nine = 9
 var twelve = 12
 
-#Booleans
+
 
 var alive = true
 var working = true
 var goofingOff = false
+
+
+
+name = "zachary"
+email = "no"
+
+console.log(name)
+console.log(working)
+console.log(goofingOff)
